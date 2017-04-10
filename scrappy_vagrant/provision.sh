cbceac1fb421cb96b4de3f5bdd7f61a899f53426
denisdbell <denisdbell@gmail.com>
2017-04-10 00:10:11 -0500

    added gitignore and updated provision

--- scrappy_vagrant/provision.sh
+++ scrappy_vagrant/provision.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+#Update repositories
+sudo apt-get update
+
 #Install scappy dependencies
-sudo apt-get install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
+sudo apt-get -y install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 
 
+#Install python
+sudo apt-get -y install python3 python3-dev
 
+#Install Scrappy
+pip install scrapy
 
 
 
