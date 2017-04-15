87f7c4e693c6330158cb545f81432952b10c4b9d
vagrant <vagrant@precise64.(none)>
2017-04-15 02:08:37 +0000

    added scrappy project

---
+++ scrapy_project/scrapy_project/spiders/jobsmart_spider.py
@@ -0,0 +1,32 @@
+import scrapy
+
+class JobSmartSpider(scrapy.Spider):
+    name = 'jobsmart'
+
+    start_urls = ['http://go-jamaica.com/jobsmart/']
+
+    def parse(self, response):
+        # follow links to job category pages
+        for href in response.xpath("//a[contains(@href,'jobtitle')]/@href").extract():
+            yield scrapy.Request(response.urljoin(href),
+                                 callback=self.parse_jobs)
+
+    #Loop through each job detail
+    def parse_jobs(self, response):
+        
+        for href in response.xpath("//a[contains(@href,'view_ad_details.php')]/@href").extract():
+             yield scrapy.Request(response.urljoin(href),callback=self.parse_job_details)
+
+
+    def parse_job_details(self, response):
+
+       yield {
+            'category': response.xpath("//td[@class='careerright']/table[2]/tr[2]/td[@class='careerdetail']/text()").extract_first()
+,
+             'title': response.xpath("//td[@class='careerright']/table[2]/tr[1]/td[@class='careerheader']/b/text()").extract_first()
+,
+             'description': response.xpath("//td[@class='careerright']/table[2]/tr[3]/td[@class='careerdetail']/node()").extract()
+,
+
+             'qualifications': response.xpath("//td[@class='careerright']/table[2]/tr[4]/td[@class='careerdetail']/node()").extract()
+}
